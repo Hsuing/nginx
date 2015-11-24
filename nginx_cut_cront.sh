cfe6bdf9e60f46183b0b0b7161de408b2bca460f
Hsuing <hxopensource@163.com>
2015-11-24 13:54:35 +0800

    Update nginx_cut_cront.sh
    second modify

--- nginx_cut_cront.sh
+++ nginx_cut_cront.sh
@@ -8,7 +8,7 @@
 #   DESCRIPTION: 
 # 
 #       OPTIONS: ---
-#  REQUIREMENTS: 第三次修改,更灵活了
+#  REQUIREMENTS: 第四次修改,更灵活了
 #          BUGS: ---
 #         NOTES: ---
 #        AUTHOR: Xiong.Han 
@@ -31,20 +31,24 @@ LOG_NAMES=(
         test.xx.com-access
         status
         )
-
+#判断目是否存在
+if [ ! -d ${BAK_PATH_LOG}$(date -d "yesterday" +"%Y")/$(date -d "yesterday" +"%m")/ ];then
+        mkdir -p ${BAK_PATH_LOG}$(date -d "yesterday" +"%Y")/$(date -d "yesterday" +"%m")/
+fi
+#
 if [ ! -d ${BAK_PATH_LOG}$(date -d "yesterday" +"%Y")/$(date -d "yesterday" +"%m")/ ];then
         mkdir -p ${BAK_PATH_LOG}$(date -d "yesterday" +"%Y")/$(date -d "yesterday" +"%m")/
 else
         echo "${BAK_PATH_LOG}$(date -d "yesterday" +"%Y")/$(date -d "yesterday" +"%m")/  存在!!"
-        continue;
+        num=${#LOG_NAMES[@]}
+
+        for((i=0; i<num; i++))
+        do
+                mv ${LOG_PATH}${LOG_NAMES[i]}.log ${BAK_PATH_LOG}$(date -d "yesterday" +"%Y")/$(date -d "yesterday" +"%m")/${LOG_NAMES[i]}_$(date -d "yesterday" +"%Y%m%d").log
+        done
+        /bin/kill -USR1 `cat ${MKTMP} 2>/dev/null` 2>/dev/null || true
 fi
 
-num=${#LOG_NAMES[@]}
 
-for((i=0; i<num; i++))
-do
-        mv ${LOG_PATH}${LOG_NAMES[i]}.log ${BAK_PATH_LOG}$(date -d "yesterday" +"%Y")/$(date -d "yesterday" +"%m")/${LOG_NAMES[i]}_$(date -d "yesterday" +"%Y%m%d").log
-done
-        /bin/kill -USR1 `cat ${MKTMP} 2>/dev/null` 2>/dev/null || true
 #rm TMP_FILE
 rm -rf $MKTMP
