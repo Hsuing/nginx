ddf00e6c9ae1b966356af9efe4a2d309f9380853
Hsuing <hxopensource@163.com>
2016-01-21 11:21:02 +0800

    Update php_waring.sh

--- php_waring.sh
+++ php_waring.sh
@@ -4,19 +4,21 @@
 
 MAIL=xxxxxx@163.com
 LOG=/var/log/nginx/error.log
-PHP_ERROR_LOG=/home/trustone/monitor_scripts/php_log.txt
+PHP_PATH=/home/trustone/monitor_scripts
+PHP_ERROR_LOG=${PHP_PATH}/php_log.txt
 PHP_RECORD=php_record.txt
-TODAY=$(date "+%Y/%m/%d %H")
+TODAY=$(date "+%Y/%m/%d %H:%m")
 
 awk '/PHP message: PHP Warning/{print $0}' $LOG > $PHP_ERROR_LOG
+awk '/connect/{print $0}' $LOG >> $PHP_ERROR_LOG
+
 egrep "$TODAY" $PHP_ERROR_LOG > $PHP_RECORD
 #egrep "^PHP message" $PHP_ERROR_LOG >> $PHP_RECORD
-
+echo "$TODAY" >> ${PHP_PATH}/record.txt
 SUM=$(cat $PHP_RECORD)
-#判断是为空，如为空，则退出
 if [ "$SUM" == "" ]
 then
         exit 1
 else
-        mail -s "PHP WARING" $MAIL < $PHP_RECORD
+        mail -s "PHP WARING 79" $MAIL < $PHP_RECORD
 fi
